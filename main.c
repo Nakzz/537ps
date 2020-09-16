41ef4a78ac7fd29a8fa5bb56522dec562ac45d59
Ajmain Naqib <ajmain@royal-13.cs.wisc.edu>
2020-09-15 19:21:22 -0500

    Genereted files with minimal structures

---
+++ main.c
@@ -0,0 +1,14 @@
+
+int main(int argc, char** argv){
+	
+	//DEVANSHU
+	//TODO:create argParser class
+	//TODO:pass in argc and argv. Expecting a map?
+	
+
+	//TODO: create a command handler which will map each flag to a function
+	//
+	//TODO: create a ps class
+	//TODO: iterate over map to call each function from the command handler
+
+}
