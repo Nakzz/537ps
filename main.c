4117d1b5f11710a5494e8b4733036e4d15389a11
Nakzz <Naqibprio10@gmail.com>
2020-09-21 02:06:44 +0000

    BUG FIX: keeping track of internal PID count

--- main.c
+++ main.c
@@ -13,7 +13,8 @@ int main(int argc, char** argv){
 
 	//
 	//create pid's for all the PID and change flags
-	int size = 5;
+	int size = 3;
+	int totalPIDs = 2;	// Everytime I initialize, I should be incrementing from 0 to keep track
 	struct pid *pid_list[size];	//TODO: change the initialization size at the size variables arg parser is giving
 	// struct pid *p2 = create_pid(20);
 	pid_list[0] = create_pid(20);
@@ -22,10 +23,9 @@ int main(int argc, char** argv){
 	pid_list[1] = create_pid(30);
 	
 	//print information about all the PIDs
-	for(int i=0; i<= size; i++){
+	for(int i=0; i< totalPIDs; i++){
 		if(pid_list[i])
 			printPID(*pid_list[i]);
-
 	}
 
 }
--- pid.c
+++ pid.c
@@ -5,51 +5,66 @@
 
     // constructor function for PID struct
 	struct pid* create_pid(int id){
-       struct pid *newPID;
 
 
         if(_pid_exists(id)){
+       struct pid *newPID;
+
         newPID = malloc(sizeof(struct pid)); //TODO: Do I have to clean this before ending my program? If should I should have a decontructor
        newPID->id = id;
-        } //TODO: should we be throwing error message if it doesn't exist?
-      
-
-       
 
        return newPID;
+
+        } //TODO: should we be throwing error message if it doesn't exist?
+        else {
+            return NULL;
+        }      
     }	
 
-    	void printPID(struct pid p){
+    void printPID(struct pid p){
         //printf(" printPID for Id is: %d  \n \n", p.id);
 
-        printf("%d: %s \n", p.id, get_flag_p(p));
+        printf("%d: %s ; Done \n", p.id, get_flag_p(p));
         //get_flag_p(p);
         //get_flag_s(p);
     }
 
     int _pid_exists(id){
         //TODO: finish implementing
-        return 1; 
+        if(id ==20)
+            return 1; 
+        
+        return 0;
     }
 
 
 	char* get_flag_p(struct pid p){
-        // char *result = malloc(sizeof(char) *20);
-        char result[100];
+        char *result = malloc(sizeof(char) *20); // allocate memory on the heap
+        //char result[100];
         
         //printf(" Id is: %d ,pFlag is: %d\n", p.id, p.flag_p);
 
         if(p.flag_p){
-            strcpy(result, "todo: p");
+            strcpy(result, "todo: p"); //TODO: Finish implementing
             
         }
 
         return result;
-
     }
 
 	char* get_flag_s(struct pid p){
         //printf(" Id is: %d ,sFlag is: %d\n", p.id, p.flag_p);
+                char *result = malloc(sizeof(char) *20); // allocate memory on the heap
+        //char result[100];
+        
+        //printf(" Id is: %d ,pFlag is: %d\n", p.id, p.flag_p);
+
+        if(p.flag_s){
+            strcpy(result, "todo: s"); //TODO: Finish implementing
+            
+        }
+
+        return result;
 
     }
 	char* get_flag_S(struct pid p){
